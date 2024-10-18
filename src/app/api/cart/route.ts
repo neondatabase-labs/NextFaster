ae4b0cb06062bc979175cba1db7258fa5859c74a
Ethan Niser <ethan@niser.com>
2024-10-18 15:40:30 -0400

    idk

--- src/app/api/cart/route.ts
+++ src/app/api/cart/route.ts
@@ -1,34 +1,33 @@
 import { db } from "@/db";
 import { getCart } from "@/lib/cart";
-import { products } from "@/db/schema";
-import { inArray } from "drizzle-orm";
 
 export const GET = async (req: Request) => {
   const cart = await getCart();
-  const dbProducts = await db
-    .select()
-    .from(products)
-    .where(
+
+  const products = await db.query.products.findMany({
+    where: (products, { inArray }) =>
       inArray(
         products.slug,
         cart.map((item) => item.productSlug),
       ),
-    );
+    with: {
+      subcategory: {
+        with: {
+          subcollection: true,
+        },
+      },
+    },
+  });
+
+  const withQuantity = products.map((product) => ({
+    ...product,
+    quantity:
+      cart.find((item) => item.productSlug === product.slug)?.quantity ?? 0,
+  }));
+
   return new Response(
     JSON.stringify({
-      cart: cart.flatMap((item) => {
-        const product = dbProducts.find((p) => p.slug === item.productSlug);
-        if (!product) {
-          return [];
-        } else {
-          return [
-            {
-              ...product,
-              quantity: item.quantity,
-            },
-          ];
-        }
-      }),
+      cart: withQuantity,
     }),
   );
 };
--- src/app/order/page.tsx
+++ src/app/order/page.tsx
@@ -1,39 +1,11 @@
-import { LoginForm } from "@/components/login-form";
-import { getCart } from "@/lib/cart";
-import type { CartItem } from "@/lib/utils";
-import Image from "next/image";
-import { db } from "@/db";
-import { products } from "@/db/schema";
-import { inArray } from "drizzle-orm";
-import Link from "next/link";
 import { Metadata } from "next";
-import { RemoveFromCartForm } from "@/components/remove-from-cart-form";
+import { CartClient } from "./cart-client";
 
 export const metadata: Metadata = {
   title: "Order",
 };
 
-export default async function Page() {
-  const cart = await getCart();
-  console.log("cart", cart);
-  const dbProducts = await db
-    .select()
-    .from(products)
-    .where(
-      inArray(
-        products.slug,
-        cart.map((item) => item.productSlug),
-      ),
-    );
-
-  const totalCost = cart.reduce(
-    (acc, item) =>
-      acc +
-      item.quantity *
-        (Number(dbProducts.find((p) => p.slug === item.productSlug)?.price) ??
-          0),
-    0,
-  );
+export default function Page() {
   return (
     <main className="min-h-screen p-4">
       <div className="container mx-auto p-3">
@@ -42,25 +14,7 @@ export default async function Page() {
         </div>
 
         <div className="grid grid-cols-3 gap-8 pt-4">
-          <div className="col-span-2">
-            {cart.length > 0 && (
-              <div className="pb-4">
-                <p className="font-semibold text-green-700">
-                  Delivers in 2-4 weeks
-                </p>
-                <p className="text-sm text-gray-500">Need this sooner?</p>
-              </div>
-            )}
-            {cart.length > 0 ? (
-              <div className="flex flex-col space-y-10">
-                {cart.map((item) => (
-                  <CartItem key={item.productSlug} item={item} />
-                ))}
-              </div>
-            ) : (
-              <p>No items in cart</p>
-            )}
-          </div>
+          <div className="col-span-2"></div>
 
           <div className="space-y-4">
             <div className="rounded bg-gray-100 p-4">
@@ -79,56 +33,5 @@ export default async function Page() {
         </div>
       </div>
     </main>
-  );
-}
-
-async function CartItem({ item }: { item: CartItem }) {
-  const product = await db.query.products.findFirst({
-    where: (products, { eq }) => eq(products.slug, item.productSlug),
-    with: {
-      subcategory: {
-        with: {
-          subcollection: true,
-        },
-      },
-    },
-  });
-  if (!product) {
-    return null;
-  }
-  return (
-    <div className="flex flex-row items-center justify-between space-x-4 border-t border-gray-200 pt-4">
-      <Link
-        href={`/products/${product.subcategory.subcollection.category_slug}/${product.subcategory.slug}/${product.slug}`}
-      >
-        <div className="flex flex-row space-x-4">
-          <div className="flex h-24 w-24 items-center justify-center bg-gray-100">
-            <Image
-              src={product.image_url ?? "/placeholder.svg"}
-              alt="Product"
-              width={80}
-              height={80}
-              quality={65}
-            />
-          </div>
-          <div className="flex-grow">
-            <h2 className="font-semibold">{product.name}</h2>
-            <p>{product.description}</p>
-          </div>
-        </div>
-      </Link>
-      <div className="flex items-center justify-center space-x-10">
-        <p>{item.quantity}</p>
-        <div className="min-w-24">
-          <p>${product.price} each</p>
-        </div>
-        <div className="min-w-24">
-          <p className="font-semibold">
-            ${Number(product.price) * item.quantity}
-          </p>
-        </div>
-        <RemoveFromCartForm productSlug={product.slug} />
-      </div>
-    </div>
   );
 }
