df76ae1c812124c7e078cdc767a656d0340a6a3b
Arman <arman.kumaraswamy@gmail.com>
2024-10-22 18:39:23 -0400

    Test no delay on prefetching images

--- src/components/ui/link.tsx
+++ src/components/ui/link.tsx
@@ -62,7 +62,7 @@ export const Link: typeof NextLink = (({ children, ...props }) => {
             }, console.error);
             // Stop observing once images are prefetched
             observer.unobserve(entry.target);
-          }, 300); // 300ms delay
+          }, 0); // No delay
         } else if (prefetchTimeout) {
           // If the element leaves the viewport before 1 second, cancel the prefetch
           clearTimeout(prefetchTimeout);
