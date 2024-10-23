4f292cae0369f0feb0a328441cc81d5134e4f6cf
Rhys Sullivan <39114868+RhysSullivan@users.noreply.github.com>
2024-10-23 11:33:35 -0700

    test: Remove onMouseDown for clicking

--- src/components/ui/link.tsx
+++ src/components/ui/link.tsx
@@ -102,20 +102,6 @@ export const Link: typeof NextLink = (({ children, ...props }) => {
         }
         setPreloading([]);
       }}
-      onMouseDown={(e) => {
-        const url = new URL(String(props.href), window.location.href);
-        if (
-          url.origin === window.location.origin &&
-          e.button === 0 &&
-          !e.altKey &&
-          !e.ctrlKey &&
-          !e.metaKey &&
-          !e.shiftKey
-        ) {
-          e.preventDefault();
-          router.push(String(props.href));
-        }
-      }}
       {...props}
     >
       {children}
