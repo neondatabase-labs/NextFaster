ff366d5aae9ecb0eb1334d675efc05e89e62e60b
Rishi Raj Jain <jain71000@gmail.com>
2025-03-21 03:35:53 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
-    ppr: true,
     inlineCss: true,
     reactCompiler: true,
   },
@@ -22,29 +21,6 @@ const nextConfig = {
         search: "",
       },
     ],
-  },
-  async rewrites() {
-    return [
-      {
-        source: "/insights/vitals.js",
-        destination:
-          "https://cdn.vercel-insights.com/v1/speed-insights/script.js",
-      },
-      {
-        source: "/insights/events.js",
-        destination: "https://cdn.vercel-insights.com/v1/script.js",
-      },
-      {
-        source: "/hfi/events/:slug*",
-        destination:
-          "https://vitals.vercel-insights.com/v1/:slug*?dsn=KD0ni5HQVdxsHAF2tqBECObqH",
-      },
-      {
-        source: "/hfi/vitals",
-        destination:
-          "https://vitals.vercel-insights.com/v2/vitals?dsn=fsGnK5U2NRPzYx0Gch0g5w5PxT1",
-      },
-    ];
   },
 };
 
